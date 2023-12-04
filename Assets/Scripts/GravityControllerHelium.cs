a016c0351a007d28bf8f9ab900ecb4fd43daadb3
ChrizzenZander <christofferssander@gmail.com>
2023-12-04 20:42:26 +0100

    Fixed OOB Blocks

--- Assets/Scripts/GravityControllerHelium.cs
+++ Assets/Scripts/GravityControllerHelium.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class GravityControllerHelium : MonoBehaviour
 {
     private GravityManager gm;
+    private Vector3 originalPosition;
+
+    [SerializeField]
+    private Transform respawnPoint;
+
+    private float maxDistanceFromStart = 200;
     private void Awake()
     {
         gm = GameObject.Find("Gravity Manager").GetComponent<GravityManager>();
+        originalPosition = transform.position;
     }
     private Rigidbody rb;
 
@@ -18,6 +25,19 @@ public class GravityControllerHelium : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.AddForce(-gm.globalGravity * 9.81f * rb.mass); // Replace 9.81 with your desired gravity strength
+        rb.AddForce(-gm.globalGravity * 9.81f * rb.mass);
+        if ((transform.position - originalPosition).magnitude >= maxDistanceFromStart) ResetTransform();
+    }
+
+    private void ResetTransform()
+    {
+        rb.velocity = Vector3.zero;
+        if (respawnPoint != null)
+        {
+            transform.position = respawnPoint.position;
+            return;
+        }
+
+        transform.position = originalPosition;
     }
 }
--- Assets/Scripts/GravityControllerMonoDirectional.cs
+++ Assets/Scripts/GravityControllerMonoDirectional.cs
@@ -6,14 +6,34 @@ public class GravityControllerMonoDirectional : MonoBehaviour
 {
     public  Vector3 gravity;
     private Rigidbody rb;
+    private Vector3 originalPosition;
+
+    [SerializeField]
+    private Transform respawnPoint;
+
+    private float maxDistanceFromStart = 200;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        originalPosition = transform.position;
     }
 
     void FixedUpdate()
     {
-        rb.AddForce(gravity * 9.81f * rb.mass); // Replace 9.81 with your desired gravity strength
+        rb.AddForce(gravity * 9.81f * rb.mass);
+        if ((transform.position - originalPosition).magnitude >= maxDistanceFromStart) ResetTransform();
+    }
+
+    private void ResetTransform()
+    {
+        rb.velocity = Vector3.zero;
+        if (respawnPoint != null)
+        {
+            transform.position = respawnPoint.position;
+            return;
+        }
+
+        transform.position = originalPosition;
     }
 }
